1a3008790bd6dcf356b0f0c4b829991c10340eab
debasish <deba.aot@gmail.com>
2026-04-10 14:17:59 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// console.log("5" + 1) //51 // type conversion  , type coersion
+// console.log("5" + 1 + 2) // 512  //"51"+2
+// console.log(5 + "1" + 2) // 512
+// console.log(5 + 1 + "2") // 62
+// console.log("5" - 1 ) //4
+// console.log("5" * 2) //10
+// console.log("6" / 2) //3
+// console.log(-1 + "5" ) 
+
+// console.log( undefined + 1) //NaN
+// console.log(typeof(NaN)) 
+// console.log(true + 1) //2
+// console.log([] + {}) //[object Object]
+// console.log([] + []) 
+// console.log(undefined + "7")
+
+var x = 5
+function sum (){
+    console.log(x)
+    var x = 7
+}
+sum()
+
